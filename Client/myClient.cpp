c94ed4b724f69e9e39072098142d6f0663af8bc2
Filipe <filipebachini@gmail.com>
2019-09-20 00:43:11 -0300

    memcpy dos guri

--- Client/myClient.cpp
+++ Client/myClient.cpp
@@ -3,66 +3,70 @@
 #include "communication.h"
 #include "sync.h"
 
-// Client side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define PORT     8000 
-#define MAXLINE 102400 
-  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from client"; 
-    struct sockaddr_in     servaddr; 
-  
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
-    memset(&servaddr, 0, sizeof(servaddr)); 
-   
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-      
-    int n; 
+// Client side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8000
+#define MAXLINE 102400
+
+// Driver code
+int main() {
+    int sockfd, i;
+    char buffer[MAXLINE];
+    char *hello = "Hello from client";
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n;
     socklen_t len = sizeof(servaddr);
 
-    
+
     packet sentPacket;
-    sentPacket.type = 0;
+    sentPacket.type = 5;
     sentPacket.seqn = 1;
     sentPacket.length = 42;
     sentPacket.total_size = 2;
-    sentPacket.checksum = 3;
-    sentPacket._payload = "oi";
+    strcpy(sentPacket._payload, "PRA QUE MARSHLING NESSA CACETA ENRICAO");
+    sentPacket.checksum = sizeof(sentPacket)*2;
 
-    
+
+    memcpy(buffer, &sentPacket, sizeof(buffer));
+
+
     char * message = "conectando";
-   // marshallPacket(&sentPacket, message);
-    printf("Depois do Marshall");
+    // marshallPacket(&sentPacket, message);
+    //printf("Depois do Marshall");
+    //fflush( stdout );
+
+
+    sendto(sockfd, (const void *) buffer, 70 , MSG_CONFIRM, (const struct sockaddr *) &servaddr,  sizeof(servaddr));  // Precisa arrumar o tamanho do que ta enviando
+    printf("Packet sent.\n");                                                                                         // 70 é só um numero cabalistico
     fflush( stdout );
 
-    sendto(sockfd, (const void *) message, sizeof(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr,  sizeof(servaddr)); 
-    printf("Packet sent.\n"); 
-    fflush( stdout );
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len); 
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
 
-    printf("Server : %s\n", buffer); 
+    printf("Server : %s\n", buffer);
     fflush( stdout );
-  
-    close(sockfd); 
-    return 0; 
-} 
+
+    close(sockfd);
+    return 0;
+}
--- Server/myServer.cpp
+++ Server/myServer.cpp
@@ -1,93 +1,103 @@
-//Headers
+ //Headers
 #include "communication.h"
 #include "fileManager.h"
 #include "sync.h"
 
-// Server side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <pthread.h>
 #include <vector>
-  
+
 
 using namespace std;
 
-#define PORT     8000 
+#define PORT     8000
 #define MAXLINE 102400
 #define MAXNUMCON 100
-  
+
 //header da thread foda-se
 void *do_it_1(void *arg);
 
 
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from server"; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family    = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(PORT); 
-      
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
-    vector<pthread_t> threads(MAXNUMCON);
-    int threadNum = 0;
-    int rc;
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family    = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //vector<pthread_t> threads(MAXNUMCON);
+    //int threadNum = 0;
+    //int rc;
 
     while(1){
             packet packetBuffer;
-            int n;
-            socklen_t len = sizeof(servaddr); 
-            n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+            int n, i;
+            socklen_t len = sizeof(servaddr);
+
+            n = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
 
 
-            char* userName = "jucaBatista";
+            memcpy(&packetBuffer, buffer, sizeof(buffer));
 
-            rc = pthread_create(&threads[threadNum], NULL, connect, (void*)userName);
+            printf("Tipo: %d\n", packetBuffer.type);
+            printf("Seq: %d\n",packetBuffer.seqn );
+            printf("Len: %d\n",packetBuffer.length );
+            printf("Size: %d\n",packetBuffer.total_size);
+            printf("Check: %d\n", packetBuffer.checksum );
+            printf("Payload: %s\n",packetBuffer._payload);
+
+            //char* userName = "jucaBatista";
+
+            //rc = pthread_create(&threads[threadNum], NULL, connect, (void*)userName);
     }
 
     /*
     unmarshallPacket(&packetBuffer, buffer);
 
-    printf("Client : %hu\n", packetBuffer.type); 
+    printf("Client : %hu\n", packetBuffer.type);
     printf("Client : %u\n", packetBuffer.total_size);
     printf("Client : %s\n", packetBuffer._payload);
 
-    sendto(sockfd, (const char *)hello, strlen(hello),  
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
-            len); 
-    printf("Hello message sent.\n");  
+    sendto(sockfd, (const char *)hello, strlen(hello),
+        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+            len);
+    printf("Hello message sent.\n");
     */
 
-    return 0; 
-} 
+    return 0;
+}
 
 
-//thread executada toda vez que abre uma 
+//thread executada toda vez que abre uma
 void *connect(void *arg) {
     printf("Thread Created Successfully!\n");
 }
